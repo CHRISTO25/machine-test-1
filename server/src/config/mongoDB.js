10f782db0978db841458cefff9b6e26b4bd6e470
CHRISTO25 <christovarghese2725@gmail.com>
2024-05-14 15:35:32 +0530

    uri

--- server/src/config/mongoDB.js
+++ server/src/config/mongoDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async ()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect("mongodb://christo:christo@ac-c68qhb9-shard-00-00.0vbji6j.mongodb.net:27017,ac-c68qhb9-shard-00-01.0vbji6j.mongodb.net:27017,ac-c68qhb9-shard-00-02.0vbji6j.mongodb.net:27017/kingdom?ssl=true&replicaSet=atlas-ognly7-shard-0&authSource=admin&retryWrites=true&w=majority");
         console.log(`MongoDB Connected : ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error:${error.message}`)
